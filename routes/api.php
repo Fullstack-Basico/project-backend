497504af2228d5594c1607c69433a8cd551664ba
mvits <stivmarquez.ud@gmail.com>
2023-06-08 08:50:44 -0500

    Example Service Laravel

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,16 @@ use Illuminate\Support\Facades\Route;
 /*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });*/
+
+
+
+Route::get("/books",[BookController::class,'read']);
+
+
+Route::post("/book",[BookController::class,'create']);
+
+
+
 
 
 Route::get("/saludo/abc",function (Request $request) {
